a0dd25f175ee1bf3574df2d5fdfdecb13a9be569
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-25 16:50:33 +0545

    arranged the border of components

--- src/component/Voice/VoiceInput.js
+++ src/component/Voice/VoiceInput.js
@@ -1,6 +1,6 @@
 export const VoiceInput=()=>{
     return(
-        <div className=" border border-black  p-3">
+        <div className=" p-3 ">
         <h1>This is VoiceInput</h1>
         </div>
     )
