324314fe0917300e6c653ee677fa5dfd1445cd60
bikash <bikashbhandari600@gmail.com>
2024-06-25 16:44:11 +0545

    my first commit

--- src/component/GPTSection/Input.js
+++ src/component/GPTSection/Input.js
@@ -4,8 +4,14 @@ export const Input=()=>{
     return(
         <>
             <div className="h-full flex flex-row justify-between w-full ">
+                <div className="h-full w-full flex ">
                 <ChatInput></ChatInput>
+                </div>
+                <div>
                 <VoiceInput></VoiceInput>
+                </div>
+                
+                
             </div>
         </>
     )
