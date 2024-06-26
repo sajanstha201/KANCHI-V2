2ff37dc59d27261facb81432bfc61635162be053
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-26 13:44:16 +0545

    done with the history bar

--- src/component/History/InstanceHistory.js
+++ src/component/History/InstanceHistory.js
@@ -1,7 +1,8 @@
 export const InstanceHistory=({data})=>{
     return(
-        <div className="mb-1 bg-gray-300 hover:bg-gray-500 overflow-hidden text-xl font-medium h-8 rounded-sm ">
-        {data}
+        <div className="mb-1 px-1 bg-gray-200 hover:bg-gray-300   h-10 rounded-sm flex items-center justify-center">
+        <div className="overflow-hidden text-[12px] font-medium h-5">{data}</div>
+        
         </div>
     )
 }
