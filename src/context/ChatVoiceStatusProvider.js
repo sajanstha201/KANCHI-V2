77389963b5f2dc3b03acd62791926e555c351374
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-27 11:30:30 +0545

    added input and ouput context

--- src/context/ChatVoiceStatusProvider.js
+++ src/context/ChatVoiceStatusProvider.js
@@ -1,11 +1,22 @@
 import {  createContext, useState,useContext } from "react"
 const chatVoiceStatusContext=createContext()
+const chatIOContext=createContext()
+const voiceIOContext=createContext()
 export const ChatVoiceStatusProvider=({children})=>{
     const [isChatActivate,setIsChatActivate]=useState(true)
+    const [chatIO,setChatIO]=useState({question:'what is you name',answer:'my name is Nepal GPT made by RAN.'})
+    const [voiceIO,setVoiceIO]=useState({})
     return(
+
         <chatVoiceStatusContext.Provider value={{isChatActivate,setIsChatActivate}}>
-            {children}
+            <chatIOContext.Provider value={{chatIO,setChatIO}}>
+                <voiceIOContext.Provider value={{voiceIO,setVoiceIO}}>
+                {children}
+                </voiceIOContext.Provider>
+            </chatIOContext.Provider>
         </chatVoiceStatusContext.Provider>
     )
 }
 export const useChatVoiceStatus=()=>useContext(chatVoiceStatusContext)
+export const useChatIO=()=>useContext(chatIOContext)
+export const useVoiceIO=()=>useContext(voiceIOContext)
